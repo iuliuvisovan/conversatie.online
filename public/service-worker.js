706a88388f380a27452ec5025171f6bb4bac86d4
Iuliu <thezingcollection2@gmail.com>
2017-09-11 01:00:02 +0300

    new service worker, no more cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,23 @@
 self.isWindowFocused = true;
 
+self.addEventListener('install', function (event) {
+    // The promise that skipWaiting() returns can be safely ignored.
+    self.skipWaiting();
+});
+
 self.addEventListener('push', event => {
     var message = event.data.json();
+    console.log('Received message!');
+    console.log(message);
 
     //If has an active window or message is received by sender
-    if (isWindowFocused || message.socketId == self.socketId)
+    if (isWindowFocused || message.socketId == self.socketId) {
+        console.log('Window focused: ' + isWindowFocused);
+        console.log('I am the sender: ' + (message.socketId == self.socketId));
         return;
+    }
+
+    console.log('Eveyrthing is cool, showing notification!: ' + message.messageText);
 
     const title = message.name;
     const options = {
@@ -18,7 +30,6 @@ self.addEventListener('push', event => {
 });
 
 self.onmessage = function (msg) {
-    console.log('received new focus value!: ' + msg.data.value);
     if (msg.data.name == 'windowFocus')
         self.isWindowFocused = msg.data.value;
     if (msg.data.name == 'socketInit')
@@ -35,27 +46,27 @@ self.addEventListener('notificationclick', function (event) {
     const url = 'https://www.conversatie.online';
 
     const urlToOpen = new URL(url, self.location.origin).href;
-      const promiseChain = clients.matchAll({
-        type: 'window',
-        includeUncontrolled: true
-      })
-      .then((windowClients) => {
-        let matchingClient = null;
-    
-        for (let i = 0; i < windowClients.length; i++) {
-          const windowClient = windowClients[i];
-          if (windowClient.url === urlToOpen) {
-            matchingClient = windowClient;
-            break;
-          }
-        }
-    
-        if (matchingClient) {
-          return matchingClient.focus();
-        } else {
-          return clients.openWindow(urlToOpen);
-        }
-      });
-    
-      event.waitUntil(promiseChain);
+    const promiseChain = clients.matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        })
+        .then((windowClients) => {
+            let matchingClient = null;
+
+            for (let i = 0; i < windowClients.length; i++) {
+                const windowClient = windowClients[i];
+                if (windowClient.url === urlToOpen) {
+                    matchingClient = windowClient;
+                    break;
+                }
+            }
+
+            if (matchingClient) {
+                return matchingClient.focus();
+            } else {
+                return clients.openWindow(urlToOpen);
+            }
+        });
+
+    event.waitUntil(promiseChain);
 });
